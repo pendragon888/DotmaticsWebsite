604cccc17a12b2f16bcf62df686e39e6c0dedfce
pendragon888 <pendragon888@hotmail.com>
2026-09-03 22:33:20 +0100

    Add smoke test to check environment is available and ready for tests

---
+++ tests/landing/smoke.setup.ts
@@ -0,0 +1,9 @@
+import { test as setup, expect } from '@fixtures/pages.fixture'
+
+setup.describe('Smoke test web application', async () => {
+    setup('Verify each page can be accessed', async ({ landingPage, page }) => {
+        await landingPage.goto()
+        await expect(page).toHaveTitle('R&D Solution for Digital Transformation')
+        await expect (landingPage.bannerHeading).toBeVisible()
+    })
+})
