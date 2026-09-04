6c4e198dc6a33e67d96610a61d986630a5ea4eac
pendragon888 <pendragon888@hotmail.com>
2026-09-04 14:47:49 +0100

    Add contact-us page smoke test

--- tests/landing/smoke.setup.ts
+++ tests/landing/smoke.setup.ts
@@ -1,9 +1,12 @@
 import { test as setup, expect } from '@fixtures/pages.fixture'
 
 setup.describe('Smoke test web application', async () => {
-    setup('Verify each page can be accessed', async ({ landingPage, page }) => {
+    setup('Verify each page can be accessed', async ({ landingPage, contactUsPage, page }) => {
         await landingPage.goto()
         await expect(page).toHaveTitle('R&D Solution for Digital Transformation')
         await expect (landingPage.bannerHeading).toBeVisible()
+        await contactUsPage.goto()
+        await expect(page).toHaveTitle('Contact Us | Dotmatics')
+        await expect (contactUsPage.bannerHeading).toBeVisible()
     })
 })
